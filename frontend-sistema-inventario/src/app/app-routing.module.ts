64b311654ae846ca9418240e154bec06deaca321
Klein Suarez <yahir.kleins@gmail.com>
2021-03-15 11:50:51 -0500

    🟢 Initial setup
    ✉️ Add spring boot project for backend and new itignore configuration.
    
    Changes to be committed:
    	new file:   .gitignore
    	new file:   frontend-sistema-inventario/.browserslistrc
    	new file:   frontend-sistema-inventario/.editorconfig
    	new file:   frontend-sistema-inventario/.gitignore
    	new file:   frontend-sistema-inventario/README.md
    	new file:   frontend-sistema-inventario/angular.json
    	new file:   frontend-sistema-inventario/e2e/protractor.conf.js
    	new file:   frontend-sistema-inventario/e2e/src/app.e2e-spec.ts
    	new file:   frontend-sistema-inventario/e2e/src/app.po.ts
    	new file:   frontend-sistema-inventario/e2e/tsconfig.json
    	new file:   frontend-sistema-inventario/karma.conf.js
    	new file:   frontend-sistema-inventario/package-lock.json
    	new file:   frontend-sistema-inventario/package.json
    	new file:   frontend-sistema-inventario/src/app/app-routing.module.ts
    	new file:   frontend-sistema-inventario/src/app/app.component.css
    	new file:   frontend-sistema-inventario/src/app/app.component.html
    	new file:   frontend-sistema-inventario/src/app/app.component.spec.ts
    	new file:   frontend-sistema-inventario/src/app/app.component.ts
    	new file:   frontend-sistema-inventario/src/app/app.module.ts
    	new file:   frontend-sistema-inventario/src/app/componentes/home/home.component.css
    	new file:   frontend-sistema-inventario/src/app/componentes/home/home.component.html
    	new file:   frontend-sistema-inventario/src/app/componentes/home/home.component.ts
    	new file:   frontend-sistema-inventario/src/app/componentes/menu/menu.component.css
    	new file:   frontend-sistema-inventario/src/app/componentes/menu/menu.component.html
    	new file:   frontend-sistema-inventario/src/app/componentes/menu/menu.component.ts
    	new file:   frontend-sistema-inventario/src/assets/.gitkeep
    	new file:   frontend-sistema-inventario/src/assets/Sistema Inventario logo v0.png
    	new file:   frontend-sistema-inventario/src/assets/inventory.png
    	new file:   frontend-sistema-inventario/src/environments/environment.prod.ts
    	new file:   frontend-sistema-inventario/src/environments/environment.ts
    	new file:   frontend-sistema-inventario/src/index.html
    	new file:   frontend-sistema-inventario/src/main.ts
    	new file:   frontend-sistema-inventario/src/polyfills.ts
    	new file:   frontend-sistema-inventario/src/styles.css
    	new file:   frontend-sistema-inventario/src/test.ts
    	new file:   frontend-sistema-inventario/tsconfig.app.json
    	new file:   frontend-sistema-inventario/tsconfig.json
    	new file:   frontend-sistema-inventario/tsconfig.spec.json
    	new file:   frontend-sistema-inventario/tslint.json
    	new file:   inventory/.gitignore
    	new file:   inventory/.mvn/wrapper/MavenWrapperDownloader.java
    	new file:   inventory/.mvn/wrapper/maven-wrapper.jar
    	new file:   inventory/.mvn/wrapper/maven-wrapper.properties
    	new file:   inventory/mvnw
    	new file:   inventory/mvnw.cmd
    	new file:   inventory/pom.xml
    	new file:   inventory/src/main/java/co/software/inventory/InventoryApplication.java
    	new file:   inventory/src/main/resources/application.properties
    	new file:   inventory/src/test/java/co/software/inventory/InventoryApplicationTests.java

---
+++ frontend-sistema-inventario/src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { HomeComponent } from './componentes/home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {path: '*', component: HomeComponent},
+  {path: 'home', component: HomeComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ frontend-sistema-inventario/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { HomeComponent } from './componentes/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
